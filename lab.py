d7cfe46e4f73f854155550b9cbb62e0256beac6b
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-03-08 14:37:38 +0100

    first python example

---
+++ lab.py
@@ -0,0 +1,6 @@
+s = "test function of tab completion"
+
+# type s.<TAB> to see the suggestions
+len(s.split(' '))
+# Show your experiments working on a string.
+# Try splitting a string into its constituent words, and count the number of words.
